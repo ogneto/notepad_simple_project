f9c05e16de409b44c6e5e3c97ff079156bec913f
Og Silva Neto <og.neto@fgv.com>
2026-02-08 20:02:30 -0300

    feat: Implement conditional header navigation based on user session and simplify the page title.

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,8 +1,15 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top shadow-sm">
     <div class="container">
+        @if(session('user'))
+        <a class="navbar-brand text-gradient" href="{{ url('/app') }}">
+            <i class="bi bi-journal-text me-2"></i>Notepad App
+        </a>
+        @else
         <a class="navbar-brand text-gradient" href="{{ url('/') }}">
             <i class="bi bi-journal-text me-2"></i>Notepad App
         </a>
+        @endif
+
         <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
--- resources/views/templates/main_template.blade.php
+++ resources/views/templates/main_template.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Notepad Project | Modern</title>
+    <title>Notepad Project</title>
 
     @include('templates.imports.default_imports')
 
